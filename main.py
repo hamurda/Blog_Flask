04f5fe0f62c4d22195737dafdfb50f91b392ead2
damla h <damlahmrc@gmail.com>
2021-07-13 14:58:28 +1000

    Generate SECRET_KEY

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
