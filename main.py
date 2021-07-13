829a8b77cbec700319133a3310f504f7792d8daa
damla h <damlahmrc@gmail.com>
2021-07-13 15:28:08 +1000

    reverts PORT

--- main.py
+++ main.py
@@ -238,5 +238,4 @@ def delete_comment(comment_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
